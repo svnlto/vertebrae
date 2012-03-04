c12573470ff319ddb0c9af8f87520117a32cdbca
Sven Lito <me@svenlito.com>
2012-03-04 20:23:40 +0000

    tweaks to bootloader

--- public/js/main.js
+++ public/js/main.js
@@ -7,33 +7,43 @@ require.config({
     text:          'libs/require/text',
     order:         'libs/require/order',
     wrap:          'libs/require/wrap',
-
     jquery:        'libs/jquery/jquery-min',
-    underscore:    'libs/underscore/underscore-min',
-    backbone:      'libs/backbone/backbone-optamd3-min'
+    underscore:    'libs/underscore/underscore',
+    backbone:      'libs/backbone/backbone'
   },
   wrapJS: {
+    "underscore": {
+      attach: "_",
+      path: 'underscore'
+    },
+    "backbone": {
+      attach: "Backbone",
+      deps: ['underscore'],
+      path: 'backbone'
+    },
     "handlebars": {
-      attach: function(){
+      attach: function() {
         var Handlebars = window.Handlebars;
             delete window.Handlebars;
             return Handlebars;
       },
       path: "libs/handlebars/handlebars"
     }
-  }
+  },
+  priority: [
+    'jquery',
+    'helpers/namespace'
+  ]
 });
 
 require([
-  'order!jquery',
-  'order!underscore',
-  'order!backbone',
-  'order!helpers/handlebars',
-  'helpers/namespace'
-],
-
-function($, _, Backbone) {
-  require([ 'helpers/config' ], function() {
-    require([ 'helpers/utils', 'router' ], function () {});
-  });
+  'wrap!underscore',
+  'wrap!backbone',
+], function($, _, Backbone) {
+  require([
+    'helpers/config',
+    'helpers/utils',
+    'order!helpers/handlebars',
+    'router'
+  ], function() {});
 });
