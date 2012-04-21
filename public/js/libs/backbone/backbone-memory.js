5f4a465e546f6fa60af5399ccf28e7715d9340d8
Sven Lito <me@svenlito.com>
2012-04-21 17:21:00 +0100

    adding AMD capabilities to BB Memory Plugin

--- public/js/libs/backbone/backbone-memory.js
+++ public/js/libs/backbone/backbone-memory.js
@@ -1,18 +1,26 @@
-//
-// this extends Backbone's sync method with simple in memory caching for reads.
-//
+//     Backbone-Memory.js 0.1.0
+//     (c) 2012 Sven Lito
+//     Licensed under the MIT license.
 
-define(['backbone'], function(Backbone) {
+  "use strict";
+
+(function (factory) {
+
+  if ( typeof define === 'function' && define.amd) {
+    //AMD
+    define(['backbone'], factory);
+  } else {
+    // CommonJS
+    factory(this.Backbone);
+  }
+}(function(Backbone) {
 
   Backbone.sync = (function(original) {
 
     return function(method, model, options) {
-
       Backbone._cache = Backbone._cache || {};
-
       var key = options.url || _.isFunction(model.url) ? model.url() : model.url;
-
-      if (method == 'read') {
+      if (method === 'read') {
         if (Backbone._cache[key]) {
           return options.success.call(this, Backbone._cache[key]);
         }
@@ -22,16 +30,14 @@ define(['backbone'], function(Backbone) {
               Backbone._cache[key] = response;
               original.call(this, response);
             };
-          })(options.success);
-
+          }(options.success));
           return original.call(this, method, model, options);
         }
-
       } else {
         return original.call(this, method, model, options);
       }
     };
 
-  })(Backbone.sync);
+  }(Backbone.sync));
 
-});
+}));
